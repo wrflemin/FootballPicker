e21819f8dd1e7c46ce7f2aae45c6fb6cc8ccd39e
Wyatt <wrflemin@gmail.com>
2016-10-01 14:27:08 -0600

    Implementing first pass of pulling average temperature data

--- picker.py
+++ picker.py
@@ -1,14 +1,21 @@
 import sys
 import re
+import urllib2
+import json
+import time
+from geopy.geocoders import Nominatim
 
 class Picker(object):	
 	def __init__(self):
 		self.line_reg_ex = '(.*\d\d)( ET | AM )(At)?(.*)'
 		self.at = 'At'
+		self.geolocator = Nominatim()
+		self.base_url = 'http://api.wunderground.com/api/{0}/almanac/q/{1},{2}.json'
+
 	def help(self):
-		print "usage: python picker.py filename.txt"
+		return "usage: python picker.py filename.txt secretApiKeyFile.txt"
 	
-	def pick(self, filename):
+	def pick(self, filename, api_secret_filename):
 		f = open(filename, 'r')
 		cities = []
 		for line in f:
@@ -17,16 +24,48 @@ class Picker(object):
 				continue
 			cities.append(self.get_cities(match.group(4).split()))
 		f.close()
+		choices = []
 		for favorite, underdog in cities:
-			print "fav: ", favorite
-			print "und: ", underdog
-	
+			favorite_avg_high = self.get_average_high(favorite, api_secret_filename)
+			underdog_avg_high = self.get_average_high(underdog, api_secret_filename)
+			if favorite_avg_high == None or underdog_avg_high == None:
+				message = 'Could not find information for cities {0} & {1}'.format(favorite, underdog)
+				choices.append(message)
+				print message
+				continue
+			if favorite_avg_high > underdog_avg_high:
+				print favorite
+				choices.append(favorite)
+			else:
+				print underdog
+				choices.append(underdog)
+			time.sleep(10) # So the free api doesn't get mad at us
+		print ", ".join(choices)
+
+	def get_average_high(self, city_name, api_secret_filename):
+		location = self.geolocator.geocode(city_name)
+		if location == None:
+			print "Location {0} not found".format(city_name)
+			return None
+			#raise Exception("Location %s not found" %city_name)
+		request_url = self.base_url.format(self.get_api_secret(api_secret_filename), location.latitude, location.longitude)
+		json_string = urllib2.urlopen(request_url).read()
+		parsed_json = json.loads(json_string)
+		return int(parsed_json['almanac']['temp_high']['normal']['C'])
+
+	def get_api_secret(self, filename):
+		with open(filename) as f:
+			return f.readline().strip('\n')
+
 	def get_cities(self, spread_line_list):
 		spread_index = self.get_spread_index(spread_line_list)
-		favorite = " ".join(self.safe_remove(spread_line_list[:spread_index], self.at))
-		underdog = " ".join(self.safe_remove(spread_line_list[spread_index+1:], self.at))
+		favorite = self.format_city_name(spread_line_list[:spread_index])
+		underdog = self.format_city_name(spread_line_list[spread_index+1:])
 		return (favorite, underdog)
 	
+	def format_city_name(self, city_name):
+		return " ".join(self.safe_remove(city_name, self.at)).replace('NY', 'New York')
+
 	def safe_remove(self, word_list, string_to_remove):
 		try:
 			word_list.remove(string_to_remove)
@@ -42,6 +81,7 @@ class Picker(object):
 
 if __name__ == "__main__":
 	picker = Picker()
-	if len(sys.argv) < 1:
+	if len(sys.argv) < 3:
 		print picker.help()
-	picker.pick(sys.argv[1])
+		exit(0)
+	picker.pick(sys.argv[1], sys.argv[2])
